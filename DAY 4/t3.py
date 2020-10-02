88d6d5a78055e2b7e0dff3a1b44d9daa38664278
hmalif <abubakaralif66@gmail.com>
2020-10-03 00:10:56 +0600

    about code

---
+++ DAY 4/t3.py
@@ -0,0 +1,9 @@
+#ইউজার একটা সংখ্যা ইনপুট দেবে। সংখ্যাটি ধনাত্মক হলে আউটপুট হবে Positive,
+#  ঋণাত্মক হলে আউটপুট হবে Negative আর জিরো হলে আউটপুট Zero হবে।
+a=int(input("Digit please.."))
+if a>0:
+    print("Positive")
+elif a<0:
+    print("Negative")
+else:
+    print("Zero")
---
+++ DAY 4/t4.py
@@ -0,0 +1,7 @@
+#ইউজার একটি পূর্ণসংখ্যা ইনপুট দেবে। সংখ্যাটি জোড় হলে আউটপুট হবে Even 
+#আর বিজোড় হলে আউটপুট Odd হবে।
+a=int(input("Enter your number..."))
+if a%2==0:
+    print("Even")
+else:
+    print("Odd")
